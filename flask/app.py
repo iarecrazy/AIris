e63a16b8ead6d66ad3a105a1b5e442bee4bd72e7
RJ <rj@rj.com>
2018-05-23 14:53:42 +0200

    added basic page for hospitals & hospital

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, abort
+from util import *
+import os
 
 app = Flask(__name__)
+app.config['root_dir'] = 'X:/'
 
 @app.route('/')
 def index():
@@ -8,15 +11,22 @@ def index():
 
 @app.route('/Hospitals')
 def list_hospitals():
-	return render_template("hospitals.html")
+	allDirs = []
+	for root, dirs, files in walklevel(app.config['root_dir'], 0):
+		for d in dirs:
+			allDirs.append(d)
 
-@app.route('/login')
-def login():
-    return 'login'
+	print(allDirs)
+	return render_template("hospitals.html", hospitals=allDirs)
 
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(username)
+@app.route('/Hospitals/<hospitalname>')
+def show_hospital(hospitalname):
+	# does directory exist?
+	path_to_hospital = os.path.join(app.config['root_dir'], hospitalname)
+	if(os.path.exists(path_to_hospital)):
+		return render_template('hospital.html', hospital=hospitalname)
+	else:
+		abort(404)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=6666, debug=True)
+	app.run()
