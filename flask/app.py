07e699af906cab4c60a6be43db28fae9293d1153
RJ <rj@rj.com>
2018-05-30 12:55:46 +0200

    added new overview & index generation

--- flask/app.py
+++ flask/app.py
@@ -3,59 +3,49 @@ import os
 from metadatautil import *
 from fileutils import *
 
-from flask import Flask, url_for, render_template, abort, send_from_directory
+from flask import Flask, url_for, render_template, abort, send_from_directory, redirect
 
 # debug = true
 
 app = Flask(__name__)
 app.config['root_dir'] = 'X:/'
 app.config['tmp_dir'] = 'C:/Temp'
+app.config['path_to_index'] = os.path.join(app.config['tmp_dir'], 'index.json')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	index = loadOrCreateIndex(app.config['root_dir'], app.config['path_to_index'])
+
+	return render_template("hospitals.html", index=index)
 
 @app.route('/Hospitals')
 def list_hospitals():
-	hospitalsFile = os.path.join(app.config['tmp_dir'], 'hospitals.json')
-	hospitals = {}
+	index = loadOrCreateIndex(app.config['root_dir'], app.config['path_to_index'])
 
-	# after one hour force refresh
-	if file_last_modified_longer_than(hospitalsFile, 60*60) is False:
-		hospitals = json_safe_read(hospitalsFile)
+	return render_template("hospitals.html", index=index)
 
-	if bool(hospitals) is False:
-		hospitals = refreshHospitals(app.config['root_dir'], hospitalsFile)
+@app.route('/GenerateIndex')
+def generate_index():
+	index = storeIndex(app.config['root_dir'], app.config['path_to_index'])
 
-	return render_template("hospitals.html", hospitals=hospitals)
-
-@app.route('/Hospitals/Refresh')
-def refresh_cache_hospitals():
-	hospitalsFile = os.path.join(app.config['tmp_dir'], 'hospitals.json')
-	hospitals = refreshHospitals(app.config['root_dir'], hospitalsFile)
-
-	return render_template("hospitals.html", hospitals=hospitals)
+	return redirect('/')
 
 @app.route('/Hospitals/<hospitalname>')
 def show_hospital(hospitalname):
-	# does directory exist?
-	path_to_hospital = os.path.join(app.config['root_dir'], hospitalname)
-	if(os.path.exists(path_to_hospital)):
+	index = loadOrCreateIndex(app.config['root_dir'], app.config['path_to_index'])
 
-		return render_template("hospital.html", hospital=hospitalname)
-	else:
-		abort(404)
+	for hospital in index['Hospitals']:
+		if hospital['Name'] == hospitalname:
+			return render_template("hospital.html", hospital=hospital)
 
-@app.route('/Hospitals/<hospitalname>/<grabberaction>/<contract>', methods=['GET', 'POST'])
+	return abort(404)
+
+@app.route('/<hospitalname>/<grabberaction>/<contract>', methods=['GET', 'POST'])
 def downloadContract(hospitalname, grabberaction, contract):
     hospitaldir 	= os.path.join(app.config['root_dir'], hospitalname)
     grabberdir 		= os.path.join(hospitaldir, grabberaction)
     contractfile 	= os.path.join(grabberdir, contract)
     return send_from_directory(grabberdir, contract, as_attachment=True)
-
-@app.route('/Hospitals/<hospitalname>/Refresh')
-def refresh_cache_hospital(hospitalname):
-	return show_hospital(hospitalname)
 
 if __name__ == '__main__':
 	app.run()
--- flask/metadatautil.py
+++ flask/metadatautil.py
@@ -1,44 +1,159 @@
 import os
 from fileutils import *
 
-def getHospitalInfo(hospitalDir):
-	info = {}
-	info['GrabberActions'] = {}
-	info['NrOfCases'] = 0
-	info['NrOfAnnotatedCases'] = 0
+def loadOrCreateIndex(path_to_root, path_to_index):
+	index = None
 
-	for rootHospital, dirs, files in walklevel(hospitalDir, 0):
-		for d in dirs:
-			info['GrabberActions'][d] = {}
-			grabberPath = os.path.join(hospitalDir, d)
+	if os.path.exists(path_to_index):
+		index = json_safe_read(path_to_index)
+	else:
+		index = storeIndex(path_to_root, path_to_index)
 
-			for rootGrabber, rooms, contracts in walklevel(grabberPath, 0):
-				info['GrabberActions'][d]['Contracts'] = contracts
+	return index
 
-				for room in rooms:
-					info['GrabberActions'][d][room] = []
-					roomPath = os.path.join(grabberPath, room)
+def storeIndex(path_to_root, path_to_index):
+	index = createNASIndex(path_to_root)
 
-					for rootRoom, cases, f in walklevel(roomPath, 0):
-						info['GrabberActions'][d][room] = cases
-						info['NrOfCases'] = info['NrOfCases'] + len(cases)
+	json_safe_dump(path_to_index, index)
 
-						for case in cases:
-							caseDirectory = os.path.join(roomPath, case)
-							if os.path.exists(os.path.join(caseDirectory, 'Annotation DL')):
-								info['NrOfAnnotatedCases'] = info['NrOfAnnotatedCases'] + 1
+	return index
 
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
 
-	return info
+def createNASIndex(hospitalRoot):
+	index = {}
+	index['GlobalStats'] = {}
+	index['GlobalStats']['NrOfCases'] = 0
+	index['GlobalStats']['NrOfAnnotatedCases'] = 0
+	index['GlobalStats']['NrOfRuns'] = 0
+	index['GlobalStats']['NrOfFXDRuns'] = 0
+	index['GlobalStats']['NrOfDICOMRuns'] = 0
+	index['GlobalStats']['NrOfAnnotatedRuns'] = 0
+	index['GlobalStats']['ProcedureTypes'] = {}
+	index['Hospitals'] = []
+	
+	globalStats = index['GlobalStats']
 
-def refreshHospitals(hospitalDir, hospitalsFile):
-	hospitals = {}
+	rootIndex = len(splitall(hospitalRoot)) - 1
+	hospitalIndex = rootIndex + 1
+	grabberIndex = hospitalIndex + 1
+	roomIndex = grabberIndex + 1
+	caseIndex = roomIndex + 1
+	annotationIndex = caseIndex + 1
+	
+	currentHospital = None
+	
+	for dirName, subdirList, files in os.walk(hospitalRoot):
+		
+		# split the dirName in its subfolder
+		directories = splitall(dirName)
+	  
+		# skip all @eaDirs
+		if '@eaDir' in directories:
+			continue
+		
+		directoryIndex = len(directories) 
+		
 
-	for root, dirs, files in walklevel(hospitalDir, 0):
+		# hospital dir
+		if directoryIndex == hospitalIndex + 1:
+			hospital = {}
+			index['Hospitals'].append(hospital)
+			
+			# get the hospital name
+			hospital['Name'] = directories[hospitalIndex]
+			hospital['NrOfCases'] = 0
+			hospital['NrOfAnnotatedCases'] = 0
+			hospital['NrOfRuns'] = 0
+			hospital['NrOfAnnotatedRuns'] = 0
+			hospital['NrOfFXDRuns'] = 0
+			hospital['NrOfDICOMRuns'] = 0
+			hospital['GrabberActions'] = {}
+			hospital['ProcedureTypes'] = {}
+			currentHospital = hospital
 
-		for d in dirs:
-			hospitals[d] = getHospitalInfo(os.path.join(hospitalDir, d))
+		elif directoryIndex == grabberIndex + 1:
+			currentHospital['GrabberActions'][directories[grabberIndex]] = {}
+			currentHospital['GrabberActions'][directories[grabberIndex]]['Contracts'] = files
 
-			json_safe_dump(hospitalsFile, hospitals)
+		elif directoryIndex == roomIndex + 1:
+			currentHospital['GrabberActions'][directories[grabberIndex]]['Rooms'] = {}
+			currentHospital['GrabberActions'][directories[grabberIndex]]['Rooms'][directories[roomIndex]] = {}
+		
+		elif directoryIndex == caseIndex + 1:
+			currentHospital['GrabberActions'][directories[grabberIndex]]['Rooms'][directories[roomIndex]][directories[caseIndex]] = {}
+			currentHospital['GrabberActions'][directories[grabberIndex]]['Rooms'][directories[roomIndex]][directories[caseIndex]]['Description'] = {}
+			
+			caseFile = os.path.join(dirName, 'case.json')
+			
+			# if there is a case file
+			if os.path.exists(caseFile):
+				# add locking
+				caseDescription = json_safe_read(caseFile) 
+				currentHospital['GrabberActions'][directories[grabberIndex]]['Rooms'][directories[roomIndex]][directories[caseIndex]]['Description'] = caseDescription 
+			
+				examType = caseDescription['Exam']['Type']
+				if examType in currentHospital['ProcedureTypes'].keys():
+					currentHospital['ProcedureTypes'][examType] += 1
+					globalStats['ProcedureTypes'][examType] += 1
+				else:
+					currentHospital['ProcedureTypes'][examType] = 1
 
-	return hospitals
+					if examType not in globalStats['ProcedureTypes'].keys():
+						globalStats['ProcedureTypes'][examType] = 1
+					else:
+						globalStats['ProcedureTypes'][examType] += 1
+
+				# count cases
+				currentHospital['NrOfCases'] += 1
+				globalStats['NrOfCases'] += 1
+		
+				for file in files:
+					if file.endswith('.fxd'):
+						hospital['NrOfFXDRuns'] += 1
+						globalStats['NrOfFXDRuns'] += 1
+						hospital['NrOfRuns'] += 1
+						globalStats['NrOfRuns'] += 1
+					elif file.startswith('IM_'):
+						hospital['NrOfDICOMRuns'] += 1
+						globalStats['NrOfDICOMRuns'] += 1
+						hospital['NrOfRuns'] += 1
+						globalStats['NrOfRuns'] += 1
+			
+		elif directoryIndex == annotationIndex + 1:
+			if dirName.endswith('Annotation DL'):
+				currentHospital['NrOfAnnotatedCases'] += 1
+				globalStats['NrOfAnnotatedCases'] += 1
+				
+				fileDict = {}
+
+				for file in files:
+					filename, ext = os.path.splitext(file)
+
+					if filename in fileDict.keys():
+						continue
+					else:
+						fileDict[filename] = True
+					
+					if ext == '.ann':
+						currentHospital['NrOfAnnotatedRuns'] += 1
+						globalStats['NrOfAnnotatedRuns'] += 1
+					elif ext == '.json':
+						currentHospital['NrOfAnnotatedRuns'] += 1
+						globalStats['NrOfAnnotatedRuns'] += 1
+		
+
+	return index
